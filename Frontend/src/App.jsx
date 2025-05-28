87791096d23f4521d54477d3dc5207d94442b4ad
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-05-28 11:58:07 +0100

    uptimised the assets folder structure by adding addition subfolder inside - icon - images etc.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import SignUp from "./pages/auth/SignUp"
 function App() {
 
   return (
     <>
-      <h1>Welcome to MEDBUDDY</h1>
+      {/* <h1 className='text-green-600'>Welcome to MEDBUDDY</h1> */}
+      <SignUp />
     </>
   )
 }
